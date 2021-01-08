38922a6a2e2fff5b1840256965c093a3f6ed78cd
Niklas Steiner <niklas@sbg.at>
2021-01-09 00:39:59 +0100

    First release

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Globe from './globe/Globe';
 
 function App() {
     return (
-        <div className="App">
+        <div className="App" style={{height: "100vh", width: "100vw"}}>
             <Globe/>
         </div>
     );
